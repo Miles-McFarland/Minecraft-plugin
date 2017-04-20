066735eae9a738da34c3b03b9fea23b621cbb149
reyeskevin9767 <reyeskevin9767@gmail.com>
2017-04-20 13:17:57 -0500

    Backpacks

---
+++ ACM-BPacks/src/backpack/Main.java
@@ -0,0 +1,87 @@
+package backpack;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Main extends JavaPlugin implements Listener{
+
+	private HashMap<UUID, Inventory> backpacks = new HashMap<UUID, Inventory>();
+	
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent e){
+		Inventory inv = Bukkit.getServer().createInventory(e.getPlayer(), 9 ,"Backpack");
+			backpacks.put(e.getPlayer().getUniqueId(), inv);
+		}
+		
+		
+
+	
+@EventHandler
+public void onPlayerLeave(PlayerQuitEvent e){
+	if(!getConfig().contains("backpacks." + e.getPlayer().getUniqueId())){
+		getConfig().createSection("backpacks." + e.getPlayer().getUniqueId());
+	}
+	char c = 'a';
+	for(ItemStack itemStack : backpacks.get(e.getPlayer().getUniqueId())){
+		if(itemStack != null){
+			saveItem(getConfig().createSection("backpacks." + e.getPlayer().getUniqueId() + "." + c++), itemStack);
+		}
+	}
+	saveConfig();
+	
+}
+public void onEnable(){
+	Bukkit.getServer().getPluginManager().registerEvents(this, this);
+}
+public void onDisable(){
+	for(Entry<UUID, Inventory> entry : backpacks.entrySet()){
+		if(!getConfig().contains("backpacks." + entry.getKey())){
+			getConfig().createSection("backpacks." + entry.getKey());
+		}
+		char c = 'a';
+		for(ItemStack itemStack : entry.getValue()){
+			if(itemStack != null){
+				saveItem(getConfig().createSection("backpacks." + entry.getKey() + "." + c++), itemStack);
+			}
+		}
+		saveConfig();
+	}
+}
+@Override
+public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+	if(!(sender instanceof Player)){
+		sender.sendMessage(ChatColor.RED + "The console cannot have an backpack.");
+		return true;
+	}
+	
+	Player p = (Player) sender;
+	
+	if(cmd.getName().equals("backpack")){
+		p.openInventory(backpacks.get(p.getUniqueId()));
+	}
+	return true;
+}
+private void saveItem(ConfigurationSection section, ItemStack itemStack){
+	section.set("type", itemStack.getType().name());
+	section.set("amount", itemStack.getAmount());
+}
+
+}
